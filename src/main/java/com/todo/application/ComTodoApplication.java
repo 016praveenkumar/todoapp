c5a1f025084ecdeed23d972f1bb656a88a195782
praveen <016praveenkumar@gmail.com>
2025-01-18 00:47:10 +0530

    first commit

---
+++ src/main/java/com/todo/application/ComTodoApplication.java
@@ -0,0 +1,13 @@
+package com.todo.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComTodoApplication.class, args);vfff
+	}
+
+}
