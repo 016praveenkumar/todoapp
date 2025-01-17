b4a68feefb6231c95ab6924158f4731d7c3b921e
praveen <016praveenkumar@gmail.com>
2025-01-18 01:09:28 +0530

    second commit

--- src/main/java/com/todo/application/ComTodoApplication.java
+++ src/main/java/com/todo/application/ComTodoApplication.java
@@ -8,7 +8,7 @@ public class ComTodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComTodoApplication.class, args);
-		//System.out.println("test");
+		System.out.println("test");
 	}
 
 }
